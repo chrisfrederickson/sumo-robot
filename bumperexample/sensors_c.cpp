29ddb134dff46d80d5cb728616340c5016789bd1
Nick <natureman1.verizon@gmail.com>
2014-09-04 21:44:28 -0400

    Cleanup
    Cleaned up the code. I still screwed up the bumperexample folder.
    
    We'll need to work on this some more.

--- bumperexample/sensors_c.cpp
+++ bumperexample/sensors_c.cpp
@@ -15,9 +15,24 @@
             traditional X, Y, Z points. Currently it's one int- the average heading over ten reads.
         -Can I find those libraries? I don't think I have them.
         -Stuff like `contact` needs calibration. What is "X" and "Y" relative to the front of the bot? 
+        
+        Tasks to complete
+        [ ] Where can I find the specs for the voltage tester?
+        [ ] For the accel, which direction is "X"?
+        [ ] Is the external LSM library included with the examples?
+        [ ] If not, is this library needed? What's up with LSM303.h and Wire.h
+        [ ] Calibrate values
+        [ ] Angle of Incidence math
+        [X] Discover exactly what versino of the accel/magne that is available: LSM303DLHC (http://www.pololu.com/catalog/product/2124)
+        [ ] There's probably other stuff to be done, like the voltage regulator specs
+        
+        https://github.com/pololu/lsm303-arduino 
+        
 ***/
 #define NUM_SENSORS 6
 #define QTR_THRESHOLD 1000 // microseconds (This is for determining white from black)
+#define ACC_THRESHOLD 1 //In odd units -- this is just something that needs plenty of calibration
+#define SLIP_THRESHOLD 2 //In odd units -- this is the acceleration detection for slippage
 
 #define CALIBRATION_SAMPLES 70 // Number of compass readings to take when calibrating
 #define CRB_REG_M_2_5GAUSS 0x60 // CRB_REG_M value for magnetometer +/-2.5 gauss full scale
@@ -29,9 +44,12 @@ Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
 LSM303 compass;
 
 typedef struct sensors_t_ {
+  //commented with suggested types instead
   unsigned int ir[6];
   float acc[3];
+  //float acc[2]
   float comp[3];
+  //float comp
   bool pushbutton;
   float vbat;
   bool contact;
@@ -43,38 +61,38 @@ typedef struct sensors_t_ {
 } sensors_t;
 // Accelerometer Class -- extends the LSM303 Library to support reading and averaging the x-y acceleration
 // vectors from the onboard LSM303DLHC accelerometer/magnetometer
-class Accelerometer : public LSM303
-{
-  typedef struct acc_data_xy
-  {
-  unsigned long timestamp;
-  int x;
-  int y;
-  float dir;
+class Accelerometer : public LSM303 {
+  typedef struct acc_data_xy {
+    unsigned long timestamp;
+    int x;
+    int y;
+    float dir;
   } acc_data_xy;
+  
   public:
-  Accelerometer() : ra_x(RA_SIZE), ra_y(RA_SIZE) {};
-  ~Accelerometer() {};
-  void enable(void);
-  void getLogHeader(void);
-  void readAcceleration(unsigned long timestamp);
-  float len_xy() const;
-  float dir_xy() const;
-  int x_avg(void) const;
-  int y_avg(void) const;
-  long ss_xy_avg(void) const;
-  float dir_xy_avg(void) const;
+    Accelerometer() : ra_x(RA_SIZE), ra_y(RA_SIZE) {};
+    ~Accelerometer() {};
+    void enable(void);
+    void getLogHeader(void);
+    void readAcceleration(unsigned long timestamp);
+    float len_xy() const;
+    float dir_xy() const;
+    int x_avg(void) const;
+    int y_avg(void) const;
+    long ss_xy_avg(void) const;
+    float dir_xy_avg(void) const;
   private:
-  acc_data_xy last;
-  RunningAverage<int> ra_x;
-  RunningAverage<int> ra_y;
+    acc_data_xy last;
+    RunningAverage<int> ra_x;
+    RunningAverage<int> ra_y;
 };
-Accelerometer lsm303;
+Accelerometer accelerometer;
 
 void setup() {
-   // Initiate LSM303
-  lsm303.init();
-  lsm303.enable();
+  Wire.begin();
+   // Initiate Accelerometer
+  accelerometer.init();
+  accelerometer.enable();
   LSM303::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32767, -32767, -32767};
   Serial.begin(9600);
    // Initiate LSM303
@@ -84,9 +102,8 @@ void setup() {
   compass.writeReg(LSM303::CRB_REG_M, CRB_REG_M_2_5GAUSS); // +/- 2.5 gauss sensitivity to hopefully avoid overflow problems
   compass.writeReg(LSM303::CRA_REG_M, CRA_REG_M_220HZ); // 220 Hz compass update rate
 
-  //How much calibration do we really need to do for a useless sensor?
-  for(index = 0; index < CALIBRATION_SAMPLES; index ++)
-  {
+  //TODO How much calibration do we really need to do for a useless sensor? Adjust num;
+  for(index = 0; index < CALIBRATION_SAMPLES; index ++) {
     // Take a reading of the magnetic vector and store it in compass.m
     compass.read();
     running_min.x = min(running_min.x, compass.m.x);
@@ -106,39 +123,24 @@ void setup() {
 void loop() {
   sensors.read(sensor_values);
   loop_start_time = millis();
-  lsm303.readAcceleration(loop_start_time); 
+  accelerometer.readAcceleration(loop_start_time); 
   //Raw Data
   sensors_t.ir = sensor_values;
-  //TODO Z
-  sensors_t.acc = lsm303.getAcceleration();
+  //TODO Z or change struct
+  sensors_t.acc = accelerometer.getAcceleration();
   //FIXME One int, not 3
   sensors_t.comp = averageHeading();
   sensors_t.pushbutton = button.isPressed();
-  //TODO Look up data
+  //TODO Look up how to do that
   sensors_t.vbat = 0;
   //Parse Data
-  sensors_t.contact = sensors_t.acc[0] > 1;
-  sensors_t.contactLeft = sensors_t.acc[1] > 1;
+  sensors_t.contact = sensors_t.acc[0] > ACC_THRESHOLD;
+  sensors_t.contactLeft = sensors_t.acc[1] > ACC_THRESHOLD;
   sensors_t.lineLeft = sensor_values[0] < QTR_THRESHOLD;
   sensors_t.lineRight = sensor_values[5] < QTR_THRESHOLD;
-  sensors_t.slip = sensors_t.acc[0] > 2;
-  //HMM - Just made something up
+  sensors_t.slip = sensors_t.acc[0] > SLIP_THRESHOLD;
+  //HMM - Just made something up. In theory some sort of trigonometric expression should correctly find the bot.
   sensors_t.angleOfIncidence = sin(sensors_t.acc[0]+sensors_t.acc[1]);
-  /*
-  typedef struct {
-		unsigned int ir[6];
-		float acc[3];
-		float comp[3];
-		bool pushbutton;
-		float vbat;
-		bool contact;
-		bool contactLeft;
-		bool lineLeft;
-		bool lineRight;
-		bool slip;
-		int angleOfIncidence;
-	} sensors_t;
-  */
 }
 
 /***
@@ -149,75 +151,67 @@ void loop() {
 // class Accelerometer -- member function definitions
 // enable accelerometer only
 // to enable both accelerometer and magnetometer, call enableDefault() instead
-void Accelerometer::enable(void)
-  {
-  // Enable Accelerometer
-  // 0x27 = 0b00100111
-  // Normal power mode, all axes enabled
-  writeAccReg(LSM303::CTRL_REG1_A, 0x27);
-  if (getDeviceType() == LSM303::device_DLHC)
-  writeAccReg(LSM303::CTRL_REG4_A, 0x08); // DLHC: enable high resolution mode
+  void Accelerometer::enable(void) {
+    // Enable Accelerometer
+    // 0x27 = 0b00100111
+    // Normal power mode, all axes enabled
+    writeAccReg(LSM303::CTRL_REG1_A, 0x27);
+    if (getDeviceType() == LSM303::device_DLHC)
+      writeAccReg(LSM303::CTRL_REG4_A, 0x08); // DLHC: enable high resolution mode
   }
-  void Accelerometer::getLogHeader(void)
-  {
-  Serial.print("millis x y len dir | len_avg dir_avg | avg_len");
-  Serial.println();
+  void Accelerometer::getLogHeader(void) {
+    Serial.print("millis x y len dir | len_avg dir_avg | avg_len");
+    Serial.println();
   }
+  //Gets the current acceleration of the device, no printing
   int[3] Accelerometer::getAcceleration(unsigned long timestamp) {
      int foo[] = {last.x, last.y};
      return foo;
   }
-  void Accelerometer::readAcceleration(unsigned long timestamp)
-  {
-  readAcc();
-  if (a.x == last.x && a.y == last.y) return;
-  last.timestamp = timestamp;
-  last.x = a.x;
-  last.y = a.y;
-  ra_x.addValue(last.x);
-  ra_y.addValue(last.y);
-  #ifdef LOG_SERIAL
-  Serial.print(last.timestamp);
-  Serial.print(" ");
-  Serial.print(last.x);
-  Serial.print(" ");
-  Serial.print(last.y);
-  Serial.print(" ");
-  Serial.print(len_xy());
-  Serial.print(" ");
-  Serial.print(dir_xy());
-  Serial.print(" | ");
-  Serial.print(sqrt(static_cast<float>(ss_xy_avg())));
-  Serial.print(" ");
-  Serial.print(dir_xy_avg());
-  Serial.println();
-  #endif
+  void Accelerometer::readAcceleration(unsigned long timestamp) {
+    readAcc();
+    if (a.x == last.x && a.y == last.y) return;
+    last.timestamp = timestamp;
+    last.x = a.x;
+    last.y = a.y;
+    ra_x.addValue(last.x);
+    ra_y.addValue(last.y);
+    #ifdef LOG_SERIAL
+    Serial.print(last.timestamp);
+    Serial.print(" ");
+    Serial.print(last.x);
+    Serial.print(" ");
+    Serial.print(last.y);
+    Serial.print(" ");
+    Serial.print(len_xy());
+    Serial.print(" ");
+    Serial.print(dir_xy());
+    Serial.print(" | ");
+    Serial.print(sqrt(static_cast<float>(ss_xy_avg())));
+    Serial.print(" ");
+    Serial.print(dir_xy_avg());
+    Serial.println();
+    #endif
   }
-  float Accelerometer::len_xy() const
-  {
-  return sqrt(last.x*a.x + last.y*a.y);
+  float Accelerometer::len_xy() const {
+    return sqrt(last.x*a.x + last.y*a.y);
   }
-  float Accelerometer::dir_xy() const
-  {
-  return atan2(last.x, last.y) * 180.0 / M_PI;
+  float Accelerometer::dir_xy() const {
+    return atan2(last.x, last.y) * 180.0 / M_PI;
   }
-  int Accelerometer::x_avg(void) const
-  {
-  return ra_x.getAverage();
+  int Accelerometer::x_avg(void) const {
+    return ra_x.getAverage();
   }
-  int Accelerometer::y_avg(void) const
-  {
-  return ra_y.getAverage();
+  int Accelerometer::y_avg(void) const {
+    return ra_y.getAverage();
   }
-  long Accelerometer::ss_xy_avg(void) const
-  {
-  long x_avg_long = static_cast<long>(x_avg());
-  long y_avg_long = static_cast<long>(y_avg());
-  return x_avg_long*x_avg_long + y_avg_long*y_avg_long;
+  long Accelerometer::ss_xy_avg(void) const {
+    long x_avg_long = static_cast<long>(x_avg());
+    long y_avg_long = static_cast<long>(y_avg());
+    return x_avg_long*x_avg_long + y_avg_long*y_avg_long;
   }
-  float Accelerometer::dir_xy_avg(void) const
-  {
-  return atan2(static_cast<float>(x_avg()), static_cast<float>(y_avg())) * 180.0 / M_PI;
+  float Accelerometer::dir_xy_avg(void) const {
+    return atan2(static_cast<float>(x_avg()), static_cast<float>(y_avg())) * 180.0 / M_PI;
   }
   // RunningAverage class
   // based on RunningAverage library for Arduino
@@ -227,56 +221,49 @@ void Accelerometer::enable(void)
   template <typename T>
   T RunningAverage<T>::zero = static_cast<T>(0);
   template <typename T>
-  RunningAverage<T>::RunningAverage(int n)
-  {
-  _size = n;
-  _ar = (T*) malloc(_size * sizeof(T));
-  clear();
+  RunningAverage<T>::RunningAverage(int n) {
+    _size = n;
+    _ar = (T*) malloc(_size * sizeof(T));
+    clear();
   }
   template <typename T>
-  RunningAverage<T>::~RunningAverage()
-  {
-  free(_ar);
+  RunningAverage<T>::~RunningAverage() {
+    free(_ar);
   }
   // resets all counters
   template <typename T>
-  void RunningAverage<T>::clear()
-  {
-  _cnt = 0;
-  _idx = 0;
-  _sum = zero;
-  for (int i = 0; i< _size; i++) _ar[i] = zero; // needed to keep addValue simple
+  void RunningAverage<T>::clear() {
+    _cnt = 0;
+    _idx = 0;
+    _sum = zero;
+    for (int i = 0; i< _size; i++) _ar[i] = zero; // needed to keep addValue simple
   }
   // adds a new value to the data-set
   template <typename T>
-  void RunningAverage<T>::addValue(T f)
-  {
-  _sum -= _ar[_idx];
-  _ar[_idx] = f;
-  _sum += _ar[_idx];
-  _idx++;
-  if (_idx == _size) _idx = 0; // faster than %
-  if (_cnt < _size) _cnt++;
+  void RunningAverage<T>::addValue(T f) {
+    _sum -= _ar[_idx];
+    _ar[_idx] = f;
+    _sum += _ar[_idx];
+    _idx++;
+    if (_idx == _size) _idx = 0; // faster than %
+    if (_cnt < _size) _cnt++;
   }
   // returns the average of the data-set added so far
   template <typename T>
-  T RunningAverage<T>::getAverage() const
-  {
-  if (_cnt == 0) return zero; // NaN ? math.h
-  return _sum / _cnt;
+  T RunningAverage<T>::getAverage() const {
+    if (_cnt == 0) return zero; // NaN ? math.h
+    return _sum / _cnt;
   }
   // fill the average with a value
   // the param number determines how often value is added (weight)
   // number should preferably be between 1 and size
   template <typename T>
-  void RunningAverage<T>::fillValue(T value, int number)
-  {
-  clear();
-  for (int i = 0; i < number; i++)
-  {
-  addValue(value);
+  void RunningAverage<T>::fillValue(T value, int number) {
+    clear();
+    for (int i = 0; i < number; i++) {
+      addValue(value);
+    }
   }
-}
 
 /***
 *                 *
@@ -288,8 +275,7 @@ void Accelerometer::enable(void)
 // want the acceleration of the Zumo to factor spuriously into the
 // tilt compensation that LSM303::heading() performs. This calculation
 // assumes that the Zumo is always level.
-template <typename T> float heading(LSM303::vector<T> v)
-{
+template <typename T> float heading(LSM303::vector<T> v) {
   float x_scaled = 2.0*(float)(v.x - compass.m_min.x) / ( compass.m_max.x - compass.m_min.x) - 1.0;
   float y_scaled = 2.0*(float)(v.y - compass.m_min.y) / (compass.m_max.y - compass.m_min.y) - 1.0;
   float angle = atan2(y_scaled, x_scaled)*180 / M_PI;
@@ -298,8 +284,7 @@ template <typename T> float heading(LSM303::vector<T> v)
   return angle;
 }
 // Yields the angle difference in degrees between two headings
-float relativeHeading(float heading_from, float heading_to)
-{
+float relativeHeading(float heading_from, float heading_to) {
   float relative_heading = heading_to - heading_from;
   // constrain to -180 to 180 degree range
   if (relative_heading > 180)
@@ -310,19 +295,15 @@ float relativeHeading(float heading_from, float heading_to)
 }
 // Average 10 vectors to get a better measurement and help smooth out
 // the motors' magnetic interference.
-float averageHeading()
-{
+float averageHeading() {
   LSM303::vector<int32_t> avg = {0, 0, 0};
-  for(int i = 0; i < 10; i ++)
-  {
-  compass.read();
-  avg.x += compass.m.x;
-  avg.y += compass.m.y;
+  for(int i = 0; i < 10; i ++) {
+    compass.read();
+    avg.x += compass.m.x;
+    avg.y += compass.m.y;
   }
   avg.x /= 10.0;
   avg.y /= 10.0;
   // avg is the average measure of the magnetic vector.
   return heading(avg);
 }
-
-
