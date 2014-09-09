65d504768d68c16fe1289d0e50c84080c484034f
Skinkworks <skinkworks42@gmail.com>
2014-09-09 18:17:39 -0400

    Made Side Impact Strategy.

---
+++ sideImpactStrategy.cpp
@@ -0,0 +1,23 @@
+#include "types.h"
+#include "drivetrain.h"
+#include <Arduino.h>
+
+unsigned long int lastSI = 0;
+
+// How to take this strategy
+void sideimpactstrategy_action(sensors_t sens){
+//  Serial.println("IMPL!");
+  leftSide(100);
+  rightSide(100);
+  return;
+}
+
+// When to take this strategy
+void sideimpactstrategy(sensors_t sens,strategy_func* taken_strat){
+  if (sens.contactLeft){
+    lastSI = millis();
+    *taken_strat = &sideimpactstrategy_action;
+  }else if (millis()-500 < lastSI){
+    *taken_strat = &sideimpactstrategy_action;
+  }
+}
--- sumo.ino
+++ sumo.ino
@@ -9,6 +9,8 @@
 #include "wallavoidance.h"
 
 #define TICK_LENGTH 10 //In milliseconds
+#include "strategies.h"
+#include "sideImpactStrategy.h"
 
 void setup(){
   Serial.begin(115200);
@@ -23,8 +25,10 @@ void loop(){
   sensors_t sens;
   // Filled
   notastrategy(sens,&cur_strategy);
+  sideimpactstrategy(sens,&cur_strategy);
   wallavoidance(sens,&cur_strategy);
+
   //Serial.println((int)&notastrategy_action);
   cur_strategy(sens);
-  while((millis - time) < TICK_LENGTH){}
+  while((millis() - time) < TICK_LENGTH){}
 }
