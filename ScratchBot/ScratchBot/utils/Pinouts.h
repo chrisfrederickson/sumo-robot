2eaeb7ae97ca5187439f6ff5fed88d9d60f3c9e7
Christopher Frederickson <chris.foody@gmail.com>
2014-11-19 19:31:28 -0500

    Actually fixed

---
+++ ScratchBot/ScratchBot/utils/Pinouts.h
@@ -0,0 +1,28 @@
+#ifndef __PINOUTS_H__
+#define __PINOUTS_H__
+
+//Motor Driver
+#define M1PWM 6
+#define M2PWM 5
+#define M1FB 18
+#define M2FB 19
+#define M1IN1 10
+#define M1IN2 11
+#define M2IN1 7
+#define M2IN2 8
+
+//IR
+#define IR_RIGHT 20
+#define IR_LEFT 21
+
+//Motion
+#define MOTION_SDA 2
+#define MOTION_SCL 3
+
+//General
+#define PROX_OUT 4
+#define BUZZER 9
+#define BUTTON 12
+#define LED 13
+
+#endif
