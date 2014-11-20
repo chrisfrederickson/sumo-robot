6032a7c593a0d898c6bba240d79c71ee321a7482
Christopher Frederickson <chris.foody@gmail.com>
2014-11-19 19:09:28 -0500

    Fixing Pin Numbers

--- ScratchBot/ScratchBot/utils/Pinout.c
+++ ScratchBot/ScratchBot/utils/Pinout.c
@@ -4,16 +4,16 @@
 //Motor Driver
 #define M1PWM 6
 #define M2PWM 5
-#define M1FB A0
-#define M2FB A1
+#define M1FB 18
+#define M2FB 19
 #define M1IN1 10
 #define M1IN2 11
 #define M2IN1 7
 #define M2IN2 8
 
 //IR
-#define IR_RIGHT A2
-#define IR_LEFT A3
+#define IR_RIGHT 20
+#define IR_LEFT 21
 
 //Motion
 #define MOTION_SDA 2
