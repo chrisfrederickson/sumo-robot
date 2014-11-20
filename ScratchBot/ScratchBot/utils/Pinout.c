5664828c14028ed6df7b3ddbfb0678575a0762da
Christopher Frederickson <chris.foody@gmail.com>
2014-11-19 19:04:07 -0500

    Add Pinouts

---
+++ ScratchBot/ScratchBot/utils/Pinout.c
@@ -0,0 +1,28 @@
+#ifndef __PINOUT_C__
+#define __PINOUT_C__
+
+//Motor Driver
+#define M1PWM 6
+#define M2PWM 5
+#define M1FB A0
+#define M2FB A1
+#define M1IN1 10
+#define M1IN2 11
+#define M2IN1 7
+#define M2IN2 8
+
+//IR
+#define IR_RIGHT A2
+#deine IR_LEFT A3
+
+//Motion
+#define MOTION_SDA 2
+#define MOTION_SCL 3
+
+//General
+#define PROX_OUT 4
+#define BUZZER 9
+#define BUTTON 12
+#define LED 13
+
+#endif
