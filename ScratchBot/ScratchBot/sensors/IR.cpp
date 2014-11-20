3e3a16fc09cb427a5303cd23bb714590b29dbae3
Christopher Frederickson <chris.foody@gmail.com>
2014-11-19 20:07:16 -0500

    Update IR.cpp
    I know what you're thinking.
    -Nick

--- ScratchBot/ScratchBot/sensors/IR.cpp
+++ ScratchBot/ScratchBot/sensors/IR.cpp
@@ -4,14 +4,20 @@
 * Created: 11/10/2014 8:50:49 PM
 * Author: N
 */
-
+#define THRESHOLD 1000
 
 #include "IR.h"
+#include "Pinouts.h"
+#include <ZumoReflectanceSensorArray>
+#include <QTRSensors.h>
+#include "Arduino.h"
 
 // default constructor
 IR::IR()
 {
-	THRESHOLD = 1000;
+	ZumoReflectanceSensorArray sensors(IR_LEFT);
+	//PINS USED ARE IR_LEFT AND IR_RIGHT
+	sensors.init();
 } 
 bool IR::detectLeft() {
 	//Detects white on the left
@@ -22,8 +28,16 @@ bool IR::detectRight() {
 	return getRightIR()>THRESHOLD;
 }
 int IR::getLeftIR() {
-	return 0;	
+	unsigned int sensorValues[6];
+	sensors.read(sensorValues);
+	return 100-sensorsValues[0]/20;
+	//I know what you're thinking -- what's up with this calibration?
+	//Well that's the stuff that we used in the original code, and
+	//that seemed to turn out pretty well. Think of this like our
+	//good luck charm. <|:-D
 }
 int IR::getRightIR() {
-	return 0;
+	unsigned int sensorValues[6];
+	sensors.read(sensorValues);
+	return 100-sensorsValues[5]/20;
 }
