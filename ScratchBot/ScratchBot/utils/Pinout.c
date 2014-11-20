0eba26d4b165f9c65fcf1b45abeebb535a296efd
Christopher Frederickson <chris.foody@gmail.com>
2014-11-19 19:13:42 -0500

    More changes

--- ScratchBot/ScratchBot/utils/Pinout.c
+++ ScratchBot/ScratchBot/utils/Pinout.c
@@ -2,27 +2,27 @@
 #define __PINOUT_C__
 
 //Motor Driver
-#define M1PWM 6
-#define M2PWM 5
-#define M1FB 18
-#define M2FB 19
-#define M1IN1 10
-#define M1IN2 11
-#define M2IN1 7
-#define M2IN2 8
+int M1PWM = 6;
+int M2PWM = 5;
+int M1FB = 18;
+int M2FB = 19;
+int M1IN1 = 10;
+int M1IN2 = 11;
+int M2IN1 = 7;
+int M2IN2 = 8;
 
 //IR
-#define IR_RIGHT 20
-#define IR_LEFT 21
+int IR_RIGHT = 20;
+int IR_LEFT = 21;
 
 //Motion
-#define MOTION_SDA 2
-#define MOTION_SCL 3
+int MOTION_SDA = 2;
+int MOTION_SCL = 3;
 
 //General
-#define PROX_OUT 4
-#define BUZZER 9
-#define BUTTON 12
-#define LED 13
+int PROX_OUT = 4;
+int BUZZER = 9;
+int BUTTON = 12;
+int LED = 13;
 
 #endif
